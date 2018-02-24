84bde90ef44785ab303e0b7a1cdb116c8d4f9b0e
gabwow <aaron.gabow@gmail.com>
2018-02-23 23:47:32 -0500

    cleaned up file reading

--- src/main/java/org/llama/domain/FileSnackStore.java
+++ src/main/java/org/llama/domain/FileSnackStore.java
@@ -3,6 +3,7 @@ package org.llama.domain;
 import java.util.List;
 import java.util.LinkedList;
 import java.util.stream.Stream;
+import java.util.stream.Collectors;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -14,21 +15,24 @@ import javax.annotation.PostConstruct;
 
 @Component
 public class FileSnackStore implements SnackStore {
-   private final List<Snack> snackPrices;
+   private List<Snack> snackPrices;
    
    
    public FileSnackStore(){
        snackPrices = new LinkedList<>();
    }
+ 
+  private Snack buildSnack(String info){
+     String[] snackValues = info.split(":");
+     return (new Snack()).name(snackValues[SNACK_INDEX])
+                     .price(Integer.parseInt(snackValues[COST_INDEX]));
+  } 
 
-   void populateFromFile(String filePath) throws IOException {
+  public void populateFromFile(String filePath) throws IOException {
        try(Stream<String> items = Files.lines(Paths.get(filePath))) {
-           items.map(line -> line.split(":")).forEach(lineInfo -> snackPrices.add(
-                (new Snack()).name(lineInfo[SNACK_INDEX])
-                             .price(Integer.parseInt(lineInfo[COST_INDEX]))
-                 ));
+           snackPrices = items.map(line ->  buildSnack(line)).collect(Collectors.toList());
        }
-   }
+  }
 
    public List<Snack> getSnackPrices(){
        return snackPrices;
